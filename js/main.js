3b46d8c717516fc8ec79fb1eb34ddb2ef221fab2
flash of flame <71262115+Laun4eroja@users.noreply.github.com>
2024-09-19 17:48:42 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,126 @@
+const tabBtnsIntro = Array.from(document.querySelectorAll(".intro__btn"));
+const tabIndicatorIntro = document.querySelector("#intro__indicator");
+const tabSlideIntro = Array.from(document.querySelectorAll(".intro__media-video"));
+const contentTitleIntro = Array.from(document.querySelectorAll(".intro__content-title"));
+
+tabBtnsIntro[0].classList.add("active");
+tabSlideIntro[0].classList.add("active");
+contentTitleIntro[0].classList.add("active");
+
+let activeBtnIntro = tabBtnsIntro[0];
+let activeSlideIntro = tabSlideIntro[0];
+let activeTitleIntro = contentTitleIntro[0];
+
+tabBtnsIntro.forEach((el) => {
+  el.addEventListener("click", onTabBtnClickIntro);
+});
+
+function onTabBtnClickIntro(e) {
+  e.preventDefault();
+  const btnIntro = e.target.closest(".intro__btn");
+  changeBtnIntro(btnIntro);
+}
+
+function changeBtnIntro(btnIntro) {
+  if (btnIntro.classList.contains("active")) {
+    return;
+  }
+  activeBtnIntro.classList.remove("active");
+  btnIntro.classList.add("active");
+  activeBtnIntro = btnIntro;
+  changeIndicatorIntro(btnIntro);
+}
+
+function changeIndicatorIntro(btnIntro) {
+  const indexBtnIntro = tabBtnsIntro.indexOf(btnIntro);
+  const indexIntro = tabBtnsIntro.indexOf(btnIntro);
+  tabIndicatorIntro.style.left = `calc(${indexBtnIntro}*100%/3)`;
+  changeSlideIntro(indexBtnIntro);
+  changeTitleIntro(indexIntro);
+}
+
+function changeSlideIntro(indexIntro) {
+  activeSlideIntro.classList.remove("active");
+  tabSlideIntro[indexIntro].classList.add("active");
+  activeSlideIntro = tabSlideIntro[indexIntro];
+}
+
+function changeTitleIntro(indexIntroTitle) {
+  activeTitleIntro.classList.remove("active");
+  contentTitleIntro[indexIntroTitle].classList.add("active");
+  activeTitleIntro = contentTitleIntro[indexIntroTitle];
+}
+
+
+
+
+
+const tabBtns = Array.from(document.querySelectorAll('.tab__btn'));
+const tabIndicator = document.querySelector(".tab__indicator");
+const tabSlide = Array.from(document.querySelectorAll(".tab__slide"));
+const root = document.querySelector(":root");
+
+const numBtns = tabBtns.length;
+
+root.style.setProperty("--num-btns", numBtns);
+
+tabBtns[0].classList.add("active");
+tabSlide[0].classList.add("active");
+
+let activeBtn = tabBtns[0];
+let activeSlide = tabSlide[0];
+
+tabBtns.forEach((el) => {
+  el.addEventListener("click", onTabBtnClick);
+});
+
+function onTabBtnClick(e) {
+  e.preventDefault();
+  const btn = e.target.closest(".tab__btn");
+  changeBtn(btn);
+}
+
+function changeBtn(btn) {
+  if (btn.classList.contains("active")) {
+    return;
+  }
+  activeBtn.classList.remove("active");
+  btn.classList.add("active");
+  activeBtn = btn;
+  changeIndicator(btn);
+}
+
+function changeIndicator(btn) {
+  const indexBtn = tabBtns.indexOf(btn);
+  tabIndicator.style.top = `calc(${indexBtn}*100%/${numBtns} + 10px)`;
+  changeSlide(indexBtn);
+}
+
+function changeSlide(index) {
+  activeSlide.classList.remove("active");
+  tabSlide[index].classList.add("active");
+  activeSlide = tabSlide[index];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+const swiperAwards = new Swiper('.awards__slider', {
+  slidesPerView: 3,
+  spaceBetween: 0,
+  loop: false,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
