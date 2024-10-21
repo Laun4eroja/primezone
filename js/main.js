88fc4120e4f63854aa6e91383a4b4d10d0f17c93
flash of flame <71262115+Laun4eroja@users.noreply.github.com>
2024-10-21 17:41:29 +0300

    add button scroll to top

--- js/main.js
+++ js/main.js
@@ -183,3 +183,23 @@ function contactsChangeSlide(index) {
   contactsActiveSlide = contactsTabSlide[index];
 }
 
+
+
+const offset = 1000;
+const scrollUp = document.querySelector('.scroll-top');
+const getTop = () => window.pageYOset || document.documentElement.scrollTop;
+
+window.addEventListener('scroll', () => {
+  if (getTop() > offset) {
+    scrollUp.classList.add('scroll-top--show');
+  } else {
+    scrollUp.classList.remove('scroll-top--show');
+  }
+});
+
+scrollUp.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
